f7aa0f981048dfed51c097375712b7364be61d22
sn0wfree <snowfreedom0815@gmail.com>
2019-04-25 08:03:28 +0800

    alter body and structure

--- tyc_finder/temp_parse.py
+++ tyc_finder/temp_parse.py
@@ -9,7 +9,7 @@ from lxml.etree import HTML, _Comment
 from tyc_finder.cookies.cookies import CookieParser
 import pandas as pd
 
-from tyc_finder.mi import other_scraper,Tianyancha
+from tyc_finder.mi import other_scraper, Tianyancha
 
 
 class ParseSinglePage(object):
@@ -53,7 +53,7 @@ class ParseCompanyBackGroundInfo(object):
             if t_getchildren:  # if not []
                 tag = t_getchildren[0].tag
                 if tag == 'img':
-                    yield t_getchildren[0].attrib
+                    yield dict(t_getchildren[0].attrib)
                 else:
                     yield t_getchildren[0].text
             else:
@@ -92,7 +92,7 @@ class ParseCompanyBackGroundInfo(object):
         """
 
         name_, company = table1_lr.xpath("""tbody/tr/td[1]//div[@class='humancompany']/div""")
-        name_dict = name_.xpath("""a""")[0].attrib
+        name_dict = dict(name_.xpath("""a""")[0].attrib)
         company_count = company.xpath("""span/text()""")[0]
         name_dict['own_company_count'] = company_count
         return name_dict
@@ -120,14 +120,21 @@ class GetContainer(object):
 
         return header, detail_list
 
-    def get_header(self):
-        pass
+    @classmethod
+    def get_back_ground_info(cls, back_ground_info):
+
+        ele = back_ground_info.xpath("""*""")[1]
+
+        # data_header, data_content = GetBaseInfoDetail._getchildren_without_comment(ele)
+
+        back_ground_info_dict = GetContainer._parser_nae_main_sector_info_router(ele)
+        return back_ground_info_dict
 
     @staticmethod
     def _parse_nav_main_baseInfo(data_header, data_content):
 
         data_title = data_header.xpath("""span[@class='data-title']/text()""")[0]
-        industry_commerce_snapshot = data_header.xpath("""span[@class='tips-block-data']/a""")[0].attrib
+        industry_commerce_snapshot = dict(data_header.xpath("""span[@class='tips-block-data']/a""")[0].attrib)
 
         table1, table2 = GetBaseInfoDetail._getchildren_without_comment(data_content)
 
@@ -138,35 +145,36 @@ class GetContainer(object):
         pass
 
     @classmethod
-    def _parser_nae_main_sector_info_router(cls, ele):
+    def _parser_nae_main_sector_info_router(cls, ele, conditions=['nav-main-baseInfo']):
 
-        conditions = ['nav-main-baseInfo',
-                      'nav-main-riskInfo',  # nav-main-riskInfo  <!--天眼风险-->
-                      'nav-main-graphTreeInfo',  # nav-main-graphTreeInfo <!--股权穿透图-->
-                      'nav-main-staffCount',  # nav-main-staffCount <!--[高管信息]-->
-                      'nav-main-holderCount',  # nav-main-holderCount <!--[股东信息]-->
-                      'nav-main-inverstCount',  # nav-main-inverstCount <!--[对外投资]-->
-                      'nav-main-shouyirenCount',  # nav-main-shouyirenCount  <!-- [最终受益人]-->
-                      'nav-main-realHoldingCount',  # nav-main-realHoldingCount 实际控制权
-                      'nav-main-graphInfo',  # nav-main-graphInfo <!-- graph 企业关系 -->
-                      'nav-main-changeCount',  # nav-main-changeCount  <!--变更信息oocss-->
-                      'nav-main-graphTimeInfo',  # nav-main-graphTimeInfo  <!-- graph 历史沿革 -->
-                      'nav-main-reportCount',  # nav-main-reportCount <!--企业年报OOCSS-->
-                      'nav-main-branchCount'  # nav-main-branchCount <!--[分支机构]-->
-                      ]
+        conditions_all = ['nav-main-baseInfo',
+                          'nav-main-riskInfo',  # nav-main-riskInfo  <!--天眼风险-->
+                          'nav-main-graphTreeInfo',  # nav-main-graphTreeInfo <!--股权穿透图-->
+                          'nav-main-staffCount',  # nav-main-staffCount <!--[高管信息]-->
+                          'nav-main-holderCount',  # nav-main-holderCount <!--[股东信息]-->
+                          'nav-main-inverstCount',  # nav-main-inverstCount <!--[对外投资]-->
+                          'nav-main-shouyirenCount',  # nav-main-shouyirenCount  <!-- [最终受益人]-->
+                          'nav-main-realHoldingCount',  # nav-main-realHoldingCount 实际控制权
+                          'nav-main-graphInfo',  # nav-main-graphInfo <!-- graph 企业关系 -->
+                          'nav-main-changeCount',  # nav-main-changeCount  <!--变更信息oocss-->
+                          'nav-main-graphTimeInfo',  # nav-main-graphTimeInfo  <!-- graph 历史沿革 -->
+                          'nav-main-reportCount',  # nav-main-reportCount <!--企业年报OOCSS-->
+                          'nav-main-branchCount'  # nav-main-branchCount <!--[分支机构]-->
+                          ]
 
         data_header, data_content = GetBaseInfoDetail._getchildren_without_comment(ele)
+
         if 'id' in ele.attrib.keys():
             nav_main_sector_name = ele.attrib['id']
-            pass
+
         elif 'id' in data_header.attrib.keys():
             nav_main_sector_name = data_header.attrib['id']
-            pass
+
         else:
             raise ValueError('cannot find id label! ')
-        if nav_main_sector_name in ['nav-main-baseInfo']:
-            ## TODO to parse more
 
+        if nav_main_sector_name in conditions:
+            ## TODO to parse more
             return getattr(cls, '_parse_{}'.format(nav_main_sector_name.replace('-', '_')))(data_header, data_content)
         else:
             return "[Error] {} is not supported yet!".format(nav_main_sector_name)
@@ -403,7 +411,7 @@ class BasicInfoParser(object):
         logo_img = logo.xpath("""div[1]/*/img[@class='img']""")[0]
 
         visitor_count = logo.xpath("""div[@class='visitor-content']/*//span[@class='pv-txt']/text()""")[0]
-        logo_dict = logo_img.attrib
+        logo_dict = dict(logo_img.attrib)
         logo_dict['visitor_count'] = visitor_count
         return logo_dict
 
@@ -488,13 +496,13 @@ class Test(object):
         return table_dict
 
     @classmethod
-    def main_path(cls, url=None):
+    def main_path(cls, url=None, public='cookies/', ):
         url = ParseSinglePage.test_url() if url is None else url
         driver = ParseSinglePage._temp_driver()
 
         driver.get(url)
         time.sleep(1)
-        CookieParser.load_refresh_cookies(driver, cookies_file='cookies.json', public='cookies/', refresh=False)
+        CookieParser.load_refresh_cookies(driver, cookies_file='cookies.json', public=public, refresh=False)
         driver.get(url)
 
         # load cookies
@@ -534,25 +542,193 @@ class Test(object):
     def parse_container(container):
         header, detail_list = GetContainer.get_container_left_tabline(container)
 
-        GetContainer.get_detail_list(detail_list)
+        container_info_list = detail_list.xpath("""//div[@class='block-data-group']""")
+        # detail_list.xpath("""//div[@class='block-data-group']""")
+
+        back_ground_info = container_info_list[0]  # <!--上市信息-->
+
+        back_ground_info_dict = GetContainer.get_back_ground_info(back_ground_info)
+        # ele = back_ground_info.xpath("""*""")[1]
+        #
+        # data_header, data_content = GetBaseInfoDetail._getchildren_without_comment(ele)
+        #
+        # back_ground_info_dict = GetContainer._parser_nae_main_sector_info_router(ele)
+
+        lawDangerous = container_info_list[1]  ##  <!--=========司法风险=============-->
+
+        knowledgeProperty = container_info_list[5]  ## <!--=========知识产权=============-->
+
+        # GetContainer.get_detail_list(detail_list)
+        return back_ground_info_dict, lawDangerous, knowledgeProperty
         pass
+
+
+class ParselawDangerous(object):
+    @staticmethod
+    def get_lawDangerous(lawDangerous):
+        pass
+
+
+class ParseTable(object):
+    @staticmethod
+    def get_tables(driver):
+        tables = driver.find_elements_by_xpath("//div[contains(@id,'_container_')]")
+
+        list_exception = ['recruit', 'tmInfo', 'holdingCompany', 'invest', 'bonus', 'firmProduct', 'jingpin',
+                          'bid', 'taxcredit', 'certificate', 'patent', 'copyright', 'product',
+                          'importAndExport',
+                          'copyrightWorks', 'wechat', 'icp', 'announcementcourt', 'lawsuit', 'court',
+                          'branch', 'touzi', 'judicialSale', 'bond', 'teamMember', 'check']
+
+        # 获取每个表格的名字
+        c = '_container_'
+        name = [0] * (len(tables) - 2)
+        # 生成一个独一无二的十六位参数作为公司标记，一个公司对应一个，需要插入多个数据表
+
+        table_dict = {}
+        for x in range(len(tables) - 2):
+            name[x] = tables[x].get_attribute('id')
+            name[x] = name[x].replace(c, '')  # 可以用这个名称去匹配数据库
+        # print(name)
+        # 判断是表格还是表单
+        # TODO: Deprecated if being tested not useful anymore.
+        # num = tables[x].find_elements_by_tag_name('table')
+
+        # 排除列表：不同业务可以设置不同分类，实现信息的精准爬取
+        # if name[x] in list_exception:
+        #     pass
+        #
+        # # 基本信息表：baseInfo，table有两个
+        # elif (name[x] == 'baseInfo') and (('baseInfo' in table) or (table == ['all'])):
+        #     print('正在爬取' + 'baseInfo')
+        #     try:
+        #         table_dict[name[x]] = get_base_info(driver)
+        #     except:
+        #         print('baseInfo表格为特殊格式，使用了标准表格爬取函数。')
+        #         table_dict[name[x]] = get_base_info(driver)
+        #
+        # # # 公司高管的特殊处理
+        # # elif name[x] == 'staff':
+        # #     table_dict[name[x]] = get_staff_info(driver)
+        #
+        # # 公告的特殊处理：加入URL
+        # elif (name[x] == 'announcement') and (('announcement' in table) or (table == ['all'])):
+        #     print('正在爬取' + 'announcement')
+        #     try:
+        #         table_dict[name[x]] = get_announcement_info(driver)
+        #     except:
+        #         print('announcement表格为特殊格式，使用了标准表格爬取函数。')
+        #         table_dict[name[x]] = get_base_info(driver)
+        #
+        # # 单纯的表格进行信息爬取
+        # # TODO: 含头像的行未对齐
+        # elif ((name[x] in table) or (table == ['all'])):
+        #     # 检查用
+        #     print('正在爬取' + str(name[x]))
+        #
+        #     df = get_table_info(tables[x])
+        #     onclickflag = tryonclick(tables[x])
+        #     ontapflag = tryontap(tables[x])
+        #     # 判断此表格是否有翻页功能
+        #     if onclickflag == 1:
+        #         df = change_page(tables[x], df, driver)
+        #     #  if ontapflag == 1:
+        #     #      df = change_tap(tables[x], df)
+        #     table_dict[name[x]] = df
+        #
+        # else:
+        #     pass
+
+
+# 标准表格爬取函数
+def get_table_info(table):
+    tab = table.find_element_by_tag_name('table')
+    df = pd.read_html('<table>' + tab.get_attribute('innerHTML') + '</table>')
+    if isinstance(df, list):
+        df = df[0]
+    if '操作' in df.columns:
+        df = df.drop(columns='操作')
+    # TODO：加入更多标准的表格处理条件
+    return df
+
+
+def tryonclick(table):  # table实质上是selenium WebElement
+    # 测试是否有翻页
+    ## 把条件判断写进tryonclick中
+    try:
+        # 找到有翻页标记
+        table.find_element_by_tag_name('ul')
+        onclickflag = 1
+    except Exception:
+        print("没有翻页")  ## 声明表格名称: name[x] +
+        onclickflag = 0
+    return onclickflag
+
+
+def tryontap(table):
+    # 测试是否有翻页
+    try:
+        table.find_element_by_xpath("//div[contains(@class,'over-hide changeTabLine f14')]")
+        ontapflag = 1
+    except Exception as e:
+        print(e)
+        print("没有时间切换页")  ## 声明表格名称: name[x] +
+        ontapflag = 0
+    return ontapflag
+
+
+change_page_interval = 10
+
+
+def change_page(table, df, driver):
+    # TODO:抽象化：频繁变换点
+    # PageCount = table.find_element_by_class_name('company_pager').text #历史class_name（天眼查的反爬措施）：'total'
+    # PageCount = re.sub("\D", "", PageCount)  # 使用正则表达式取字符串中的数字 ；\D表示非数字的意思
+    PageCount = len(table.find_elements_by_xpath(".//ul[@class='pagination']/li")) - 1
+
+    for _ in range(int(PageCount) - 1):
+        # TODO:抽象化：频繁变换点
+        button = table.find_element_by_xpath(
+            ".//a[@class='num -next']")  # 历史class_name（天眼查的反爬措施）：'pagination-next  ',''
+        driver.execute_script("arguments[0].click();", button)
+        ####################################################################################
+        time.sleep(change_page_interval)  # 更新换页时间间隔,以应对反爬虫
+        ####################################################################################
+        df2 = get_table_info(table)  ## 应该可以更换不同的get_XXXX_info
+        df = df.append(df2)
+    return df
+
+
+def standard_table_extractor(name):
+    # 检查用
+    print('正在爬取' + str(name[x]))
+
+    df = get_table_info(tables[x])
+    onclickflag = tryonclick(tables[x])
+    ontapflag = tryontap(tables[x])
+    # 判断此表格是否有翻页功能
+    if onclickflag == 1:
+        df = change_page(tables[x], df, driver)
+    #  if ontapflag == 1:
+    #      df = change_tap(tables[x], df)
+    table_dict[name[x]] = df
 
 
 if __name__ == '__main__':
     l0, basic_info, tyc_risk, l3, container, driver = Test.main_path()
     logo_dict, simple_info = Test.parse_basic_info(basic_info)
-    header, detail_list = GetContainer.get_container_left_tabline(container)
+    # header, detail_list = GetContainer.get_container_left_tabline(container)
 
-    knowledgeProperty = detail_list.xpath("""//div[@class='block-data-group']""")[5]
-    lawDangerous = detail_list.xpath("""//div[@class='block-data-group']""")[1]
-    element = detail_list.xpath("""//div[@class='block-data-group']""")[0]
-    ele = element.xpath("""*""")[1]
+    # knowledgeProperty = detail_list.xpath("""//div[@class='block-data-group']""")[5]  # knowledgeProperty
+    # lawDangerous = detail_list.xpath("""//div[@class='block-data-group']""")[1]  # lawDangerous
+    # element = detail_list.xpath("""//div[@class='block-data-group']""")[0]
+    # ele = element.xpath("""*""")[1]
+    #
+    # data_header, data_content = GetBaseInfoDetail._getchildren_without_comment(ele)
+    #
+    # back_ground_info_dict = GetContainer._parser_nae_main_sector_info_router(ele)
 
-    data_header, data_content = GetBaseInfoDetail._getchildren_without_comment(ele)
-
-    back_ground_info_dict = GetContainer._parser_nae_main_sector_info_router(ele)
-
-
+    back_ground_info_dict, lawDangerous, knowledgeProperty = Test.parse_container(container)
 
     print(1)
     pass
